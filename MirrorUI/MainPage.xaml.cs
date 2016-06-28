9bf58b97a2e9ae601d348d611e9b899369376d27
Pospa <pospa@pospa.net>
2016-06-28 23:41:32 +0200

    Face detection

--- MirrorUI/MainPage.xaml.cs
+++ MirrorUI/MainPage.xaml.cs
@@ -1,46 +1,78 @@
 ﻿using System;
-using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Windows.Devices.Enumeration;
 using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml.Controls;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
 
 namespace Pospa.NET.MagicMirror.UI
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
-        private readonly MediaCapture _mediaCapture;
+        private const string OxfordApiKey = "<API key>";
+        private const string PersonGroupId = "<person group ID>";
+        private const double MinConfidenceLevel = 75;
+
+        private readonly CancellationTokenSource _cancellationTokenSource;
 
         public MainPage()
         {
-            this.InitializeComponent();
-            InitializeSensorsAsync();
-            InitializeCameraAsync();
+            _cancellationTokenSource = new CancellationTokenSource();
+            InitializeComponent();
+            InitializeCameraAsync()
+                .ContinueWith(
+                    mediaCaptureTask => InitMirrorAsync(mediaCaptureTask.Result, _cancellationTokenSource.Token));
         }
 
-        private async Task InitializeSensorsAsync()
+        private async Task InitMirrorAsync(MediaCapture mediaCapture, CancellationToken cancellationToken)
         {
+            FaceServiceClient client = new FaceServiceClient(OxfordApiKey);
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                InMemoryRandomAccessStream photoStream = new InMemoryRandomAccessStream();
+                await mediaCapture.CapturePhotoToStreamAsync(ImageEncodingProperties.CreateJpeg(), photoStream);
+                Face[] faces = await client.DetectAsync(photoStream.AsStreamForRead());
+                IdentifyResult[] identifyResults =
+                    await client.IdentifyAsync(PersonGroupId, faces.Select(face => face.FaceId).ToArray());
+                Guid[] persons =
+                    identifyResults.Where(
+                        result =>
+                            result.Candidates.OrderByDescending(candidate => candidate.Confidence).First().Confidence >
+                            MinConfidenceLevel)
+                        .Select(
+                            result =>
+                                result.Candidates.OrderByDescending(candidate => candidate.Confidence).First().PersonId)
+                        .ToArray();
+                Person[] allPersons = await client.GetPersonsAsync(PersonGroupId);
+                foreach (Person person in allPersons.Where(person => persons.Contains(person.PersonId)))
+                {
+                    //ToDo
+                }
+            }
         }
 
         private async Task<MediaCapture> InitializeCameraAsync()
         {
             DeviceInformationCollection allVideoDevices = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
-            DeviceInformation cameraDevice =allVideoDevices.FirstOrDefault();
+            DeviceInformation cameraDevice = allVideoDevices.FirstOrDefault();
 
             if (cameraDevice == null)
             {
                 return null;
             }
 
-            var mediaCapture = new MediaCapture();
+            MediaCapture mediaCapture = new MediaCapture();
 
-            var mediaInitSettings = new MediaCaptureInitializationSettings {VideoDeviceId = cameraDevice.Id};
+            MediaCaptureInitializationSettings mediaInitSettings = new MediaCaptureInitializationSettings
+            {
+                VideoDeviceId = cameraDevice.Id
+            };
 
             try
             {
@@ -57,5 +89,4 @@ namespace Pospa.NET.MagicMirror.UI
             return mediaCapture;
         }
     }
-
 }
